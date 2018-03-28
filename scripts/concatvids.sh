e2222a54c2159c94d206b4b4e0754416a7230ae9
esluyter <wondersluyter@gmail.com>
2018-03-28 18:09:24 -0400

    fix nested directories containing .MP4 files

--- scripts/concatvids.sh
+++ scripts/concatvids.sh
@@ -2,7 +2,9 @@
 
 file=$(echo ${PWD##*/} | tr -d '[:punct:]')
 
-printf "file '%s'\n" ./*.MP4 > list.txt
+mv */*.MP4 .
+rmdir *
+printf "file '%s'\n" *.MP4 > list.txt
 ffmpeg -f concat -safe 0 -i list.txt -c:v copy "$file.mp4"
 rm list.txt
 mkdir to_delete
