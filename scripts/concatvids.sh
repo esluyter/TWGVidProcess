caf1b243dd81aeacfad721ec08176a98480d97da
esluyter <wondersluyter@gmail.com>
2018-06-04 16:13:00 -0400

    check for to_delete folder before creating it

--- scripts/concatvids.sh
+++ scripts/concatvids.sh
@@ -7,5 +7,6 @@ rmdir *
 printf "file '%s'\n" *.MP4 > list.txt
 ffmpeg -f concat -safe 0 -i list.txt -c:v copy "$file.mp4"
 rm list.txt
-mkdir to_delete
+
+[[ -d to_delete ]] || mkdir to_delete
 mv *.MP4 to_delete
